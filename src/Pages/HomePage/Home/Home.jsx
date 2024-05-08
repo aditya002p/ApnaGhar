5f05567af98a51c211e8f9c4a21b985d86f4ba21
Aditya <ricky.pathak902@gmail.com>
2024-05-09 04:28:44 +0530

    first commit for this project

---
+++ src/Pages/HomePage/Home/Home.jsx
@@ -0,0 +1,31 @@
+import { Helmet } from "react-helmet";
+import SwiperSliders from "../SwiperSliders/SwiperSliders";
+import RecentNews from "../RecentNews/RecentNews";
+import Subscribe from "../Subscribe/Subscribe";
+import OurAgents from "../OurAgents/OurAgents";
+import PropertyTypes from "../PropertyTypes/PropertyTypes";
+import AdvertiseProperty from "../AdvertiseProperty/AdvertiseProperty";
+import LatestReview from "../LatestReview/LatestReview";
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Evergreen Estate | Home</title>
+      </Helmet>
+
+      <div>
+        <SwiperSliders></SwiperSliders>
+        <AdvertiseProperty></AdvertiseProperty>
+        <OurAgents></OurAgents>
+        <PropertyTypes></PropertyTypes>
+        <LatestReview></LatestReview>
+        <RecentNews></RecentNews>
+        <Subscribe></Subscribe>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
